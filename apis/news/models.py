49753d1b9e9896e564e60b6200394cf8d616aaba
premanuj <mesubedianuj@gmail.com>
2019-09-08 09:58:33 +0545

    Jwt authentication using simple jwt

--- apis/news/models.py
+++ apis/news/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from apis.accounts.models import User
 
 # Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    story = models.TextField()
+    reporter = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- apis/news/views.py
+++ apis/news/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework_simplejwt import authentication
+from rest_framework import permissions
+from apis.news.serializers import NewsSerializer
+
 
 # Create your views here.
+
+class CreateNewsAPIView(generics.CreateAPIView):
+    authentication_classes = (authentication.JWTAuthentication,)
+    serializer_class = NewsSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+        
