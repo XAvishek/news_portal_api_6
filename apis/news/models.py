0d0d70307fca378ea74690d632ff8021bd7193b8
premanuj <mesubedianuj@gmail.com>
2019-09-09 09:35:36 +0545

    CRUD operations in news app

--- apis/news/models.py
+++ apis/news/models.py
@@ -4,8 +4,10 @@ from apis.accounts.models import User
 # Create your models here.
 
 class News(models.Model):
+    CATEGORY = (("0", "Politics"), ("1", "Sports"), ("2", "Fashoin"), ("3", "Technology"))
     title = models.CharField(max_length=255)
     story = models.TextField()
+    category = models.CharField(choices=CATEGORY, max_length=2)
     reporter = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- apis/news/views.py
+++ apis/news/views.py
@@ -3,7 +3,10 @@ from rest_framework import generics
 from rest_framework_simplejwt import authentication
 from rest_framework import permissions
 from apis.news.serializers import NewsSerializer
-
+from rest_framework.decorators import api_view
+from apis.news.models import News
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
@@ -12,4 +15,36 @@ class CreateNewsAPIView(generics.CreateAPIView):
     serializer_class = NewsSerializer
     permission_classes = (permissions.IsAuthenticated,)
 
-        
+class ListNewsAPIView(generics.ListAPIView):
+    serializer_class = NewsSerializer
+    permission_classes = (permissions.AllowAny,)
+    queryset = News.objects.all()
+
+class DetailNewsAPIView(generics.RetrieveAPIView):
+    serializer_class = NewsSerializer
+    permission_classes = (permissions.AllowAny,)
+    queryset = News.objects.all()
+
+
+    # def get_queryset(self):
+    #     return News.objects.all()
+    
+
+class DeleteNewsAPIView(generics.DestroyAPIView):
+    authentication_classes = (authentication.JWTAuthentication,)
+    serializer_class = NewsSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = News.objects.all()
+
+
+class UpdateNewsAPIView(generics.UpdateAPIView):
+    authentication_classes = (authentication.JWTAuthentication,)
+    serializer_class = NewsSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = News.objects.all()
+
+
+@api_view(['GET', 'POST'])
+def get_category_list(request):
+    categories = dict(News.CATEGORY)
+    return Response(categories, status=status.HTTP_200_OK)
